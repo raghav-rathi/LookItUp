f7ebe2dfaea7b677917a4cc316c32ef26f13a73b
dakshmehta23 <45225674+dakshmehta23@users.noreply.github.com>
2022-02-04 18:15:49 -0500

    created front-end react app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <>
+    <Header/>
+   <h1>Testing success working</h1>
+   <Footer/>
+   </>
+  );
+}
+
+export default App;
