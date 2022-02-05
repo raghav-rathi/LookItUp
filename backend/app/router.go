4e851eeabb3c456514c1e10b9d709c0e6604fe25
Suneet Jain <suneet.bhopal@gmail.com>
2022-02-04 21:38:36 -0500

    Sql connectivity

--- backend/app/router.go
+++ backend/app/router.go
@@ -1,5 +1,8 @@
 package app
 
+import "github.com/raghav-rathi/LookItUp/backend/controller/users"
+
 func mapUrls() {
 	router.POST("/api/register", users.Register)
+	router.POST("/api/login", users.Login)
 }
--- backend/datasource/mysql/users_db/users_db.go
+++ backend/datasource/mysql/users_db/users_db.go
@@ -11,18 +11,27 @@ import (
 var (
 	Client   *sql.DB
 	username = "root"
-	password = ""
 	host     = "127.0.0.1:3306"
-	schema   = "GoReactAuth"
+	schema   = "users"
+	password = "10102000"
 )
 
 func init() {
+	// envErr := godotenv.Load()
+	// if envErr != nil {
+	// 	log.Fatal("Error loading .env file")
+	// }
+	// password := os.Getenv("DB_PASSWORD")
+
+	// username:password@tcp(host)/user_schema
 	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8", username, password, host, schema)
+
 	var err error
-	Client, err = sql.Open("mysql", datSourceName)
+	Client, err = sql.Open("mysql", dataSourceName)
 	if err != nil {
 		panic(err)
 	}
+
 	if err := Client.Ping(); err != nil {
 		panic(err)
 	}
