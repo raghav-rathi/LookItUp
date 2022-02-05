35b9b3741d3b0b44a543140fc9b6da10c6849533
dakshmehta23 <45225674+dakshmehta23@users.noreply.github.com>
2022-02-04 21:42:24 -0500

    Front-end nav bar with routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,15 +2,27 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header'
 import Footer from './components/Footer'
+import {Container} from 'react-bootstrap'
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/LoginScreen';
+import SignupScreen from './screens/SignupScreen';
 
 function App() {
   return (
-    <>
+    <Router>
      <Header/>
-     <h1>Testing react app</h1>
+     <main>
+      <Container> 
+      <Route path='/' component={HomeScreen}/>
+      <Route path='/signup' component={SignupScreen}/>
+      <Route path='/login' component={LoginScreen}/>
+     </Container>
+     </main>
      <Footer/>
-     </>
-  );
+     </Router>
+  )
 }
 
 export default App;
+
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import {Nav,Navbar,Button,Container} from 'react-bootstrap'
+
 
 const Header = () => {
-  return <div>This is the header</div>;
-};
+  return(<Navbar bg="dark" variant='dark' collapseOnSelect expand="lg">
+  <Container>
+    <Navbar.Brand href="/">LookItUp</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="ms-auto">
+        <Nav.Link href="/signup">Sign Up</Nav.Link>
+        <Nav.Link href="/login">Login</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>)  
+}
 
 export default Header;
