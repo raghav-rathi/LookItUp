9b74920e5245238d2ce1f0ade4674196900804bb
dakshmehta23 <45225674+dakshmehta23@users.noreply.github.com>
2022-04-01 22:00:13 -0400

    Front end for the about us page

--- newcomponent_scanner/scannerfrontend/src/components/about/About.jsx
+++ newcomponent_scanner/scannerfrontend/src/components/about/About.jsx
@@ -3,7 +3,52 @@ import React from 'react'
 const About = () => {
   return (
     <div>
-        about us page
+        <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous"></link>
+    </head>
+    <main role="main">
+<div class="jumbotron">
+  <div class="container">
+    <h1 class="display-3">FirstGear</h1>
+    <p>As the name suggests, FirstGear is the first and the last destination you will need to visit to
+fulfill your car’s demand for its spare parts. FirstGear is an online marketplace where the
+user can simply search for his/her vehicle by their model and the type of spare part they
+require, such as: Brake Pads, Air filters, Engine Oil, and many more. All these spares are
+procured from the official manufacturers and are 100% genuine. A user can access
+FirstGear by simply entering our website URL in their web browser and accessing our
+website and once they add the spare parts to the cart, all they need to do is enter their
+address and voila! The order will be placed and shipped and the car will be running as
+smoothly as ever once the new parts are installed.</p>
+    <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more »</a></p>
+  </div>
+</div>
+
+<div class="container">
+  
+  <div class="row">
+    <div class="col-md-4">
+      <h2>Home Delivery</h2>
+      <p>
+        Select the products you want to buy for your car and add them to your cart. Enter the address you want them to be delivered at and our delivery agents will make sure to ship the products within <b>5-7 business days.</b>
+      </p>
+    </div>
+    <div class="col-md-4">
+      <h2>Payment</h2>
+      <p>To limit the spread of the novel coronavirus (COVID-19) we are currently accepting online payments only. You will be redirected to our partner to complete your transaction when you click on the place order button at the checkout page.</p>
+    </div>
+    <div class="col-md-4">
+      <h2>Contact Us</h2>
+      <p>If you have any queries regarding the service provided my FirstGear, we would be happy to help you out. Simply click on the contact us button present at the top of the page and fill out the form and our team will contact you to provide you with the best service possible. </p>
+    </div>
+  </div>
+
+</div> 
+
+</main>    
+
+
+
+
     </div>
   )
 }
