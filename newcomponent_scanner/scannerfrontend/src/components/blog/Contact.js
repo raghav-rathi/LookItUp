5b0ecafc02865fcfba57260f5f7150ffa2c1a139
raghav-rathi <raghavology2.0@gmail.com>
2022-04-01 23:19:20 -0400

    added contact section on naviagation bar

--- newcomponent_scanner/scannerfrontend/src/components/blog/Contact.js
+++ newcomponent_scanner/scannerfrontend/src/components/blog/Contact.js
@@ -3,8 +3,10 @@ import React from 'react'
 export const Contact = () => (
   <div>
     <h2>Contact Us</h2>
+    
     <p>Email: lookitup@samplemail.com</p>
     <p>Contact No: +1 3333355555</p>
+    <p><Address: 37th Blvd, Gainesville </p>
 
   </div>
 )
