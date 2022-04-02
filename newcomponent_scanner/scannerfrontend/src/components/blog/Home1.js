e2d2b199804cb2826b5aeae3ecf8c9d553301644
raghav-rathi <raghavology2.0@gmail.com>
2022-04-02 00:08:12 -0400

    updated ingredient database link

--- newcomponent_scanner/scannerfrontend/src/components/blog/Home1.js
+++ newcomponent_scanner/scannerfrontend/src/components/blog/Home1.js
@@ -17,6 +17,14 @@ export const Home = () => (
     <br></br>
     <h3>Ethylhexyl Dimethyl PABA</h3>
     <p>Ethylhexyl Dimethyl PABA is a chemical sunscreen agent that protects in the UVB range (290-320 nm) with a peak absorbance at 312nm. Other than that, we struggle to write anything positive about this guy, as it is photo unstable (looses 10% efficacy in 20 mins and 50% in 85 mins), not an unfrequent allergen in sunscreens and shows some endocrine effect in animal studies. All this does not mean that it is not considered safe as used, it is (and is approved up to 8% in the EU and US and up to 3% in Japan), but we do think that there are plenty of other and better UV filters out there.  </p>
+    <h3>Rosa Damascena Flower Oil</h3>
+    <p>We are big fans of all kinds of roses as ornamental plants but when it comes to skincare, it is a mixed bag. Before we list out the good and the not so good, here is an interesting thing. The oil content in rose is very, very low so distilling rose essential oil requires huge amounts of rose flowers. It has such a wonderful scent that there are no comparable synthetic alternatives. You can probably guess that this means rose essential oil is expensive.... very very expensive.</p>
+    <br></br>
+    <h3>Palmitoyl Tetrapeptide-7</h3>
+    <p>A four amino acid peptide with the amino sequence of glycine-glutamine-proline-arginine. It is attached to palmitic acid (a fatty acid)  to increase oil solubility and skin penetration. It works by reducing the production of the signal molecule, interleukin-6 (IL-6) which promotes inflammation in the skin and less inflammation means slower degradation of important things (like collagen) that results in younger looking skin for a longer time.   </p>
+    <br></br>
+    <h3>Gluconolactone</h3>
+    <p>Gluconolactone is kind of an acid, even though you would probably not guess it from its name. It’s a PHA, or polyhydroxy acid. PHAs are close relatives to the famous AHAs, both in terms of similar chemical structure and similar effects on the skin. They are often marketed as new-generation AHAs. So if AHAs do not tell you anything, you can read all about them here. Go and read. We tell you, it’s worth the time. </p>
 
   </div>
 )
