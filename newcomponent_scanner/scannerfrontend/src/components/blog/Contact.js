df91e3a1c0eb1370c60c4ae164161a3931da3e05
raghav-rathi <raghavology2.0@gmail.com>
2022-04-01 23:13:21 -0400

    Blog page push containing descriptions of ingredients
    Blog page push which contains detailed description about each ingredients in our database.

---
+++ newcomponent_scanner/scannerfrontend/src/components/blog/Contact.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Contact = () => (
+  <div>
+    <h2>Contact Us</h2>
+    <p>Email: lookitup@samplemail.com</p>
+    <p>Contact No: +1 3333355555</p>
+
+  </div>
+)
---
+++ newcomponent_scanner/scannerfrontend/src/components/blog/Home1.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export const Home = () => (
+  <div>
+    <h1>Ingredients Newly Explained</h1><br></br>
+    <h3>PPG-15 Stearyl Ether Benzoate</h3>
+    <p>An emollient liquid that usually comes to the formula combined with C12-15 Alkyl Benzoate and Dipropylene Glycol Dibenzoate as the trio works in synergy to solubilize hard to dissolve sunscreen agents while providing a nice, non-greasy skin feel. </p>
+    <br></br>
+    <h3>PPG-15 Stearyl Ether Benzoate</h3>
+    <p>Acrylates/C12-22 Alkyl Methacrylate Copolymer is a big molecule created from repeating subunits (aka polymer) used as a film-former. It is great at improving the water and wear resistance of sunscreens and makeup products. </p>
+    <br></br>
+    <h3>Dipropylene Glycol Dibenzoate</h3>
+    <p>An oily liquid that makes your skin nice and smooth (aka emollient). It is also claimed to have some humectant and moisturizing properties without a greasy after-feel. But its real superpower is being an outstanding solvent for hard to solubilize sunscreen agents (that is most of the chemical sunscreen filters) making it an excellent emollient choice in high SPF products.  </p>
+    <br></br>
+    <h3>Sodium Phenylbenzimidazole Sulfonate</h3>
+    <p>The salt form of the water-soluble chemical sunscreen, Ensulizole. What we wrote there (UVB filter with a peak protection at 306 nm), applies here as well. In fact, the pure acid form (aka Phenylbenzimidazole Sulfonic Acid) can be incorporated into a sunscreen formula only if it is neutralized with a base (otherwise it is not water-soluble), so even if the acid form in on the ingredient list, what you have in the formula is a salt form, just like this one.  </p>
+    <br></br>
+    <h3>Ethylhexyl Dimethyl PABA</h3>
+    <p>Ethylhexyl Dimethyl PABA is a chemical sunscreen agent that protects in the UVB range (290-320 nm) with a peak absorbance at 312nm. Other than that, we struggle to write anything positive about this guy, as it is photo unstable (looses 10% efficacy in 20 mins and 50% in 85 mins), not an unfrequent allergen in sunscreens and shows some endocrine effect in animal studies. All this does not mean that it is not considered safe as used, it is (and is approved up to 8% in the EU and US and up to 3% in Japan), but we do think that there are plenty of other and better UV filters out there.  </p>
+
+  </div>
+)
