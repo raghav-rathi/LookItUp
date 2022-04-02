013ceb895691531291b01f5fce94692138ba0a51
raghav-rathi <raghavology2.0@gmail.com>
2022-04-01 23:23:31 -0400

    added frontend support for aws database shift from sql

--- newcomponent_scanner/scannerfrontend/src/components/blog/components1/Layout1.js
+++ newcomponent_scanner/scannerfrontend/src/components/blog/components1/Layout1.js
@@ -3,6 +3,7 @@ import { Container } from 'react-bootstrap';
 
 export const Layout = (props) => (
   <Container>
+    {/* aws.serverconnect= constlayer.pull(aws.getconnect()) */}
     {props.children}
   </Container>
 )
