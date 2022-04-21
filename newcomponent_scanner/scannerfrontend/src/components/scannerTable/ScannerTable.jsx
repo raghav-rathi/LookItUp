e409bac110fe40e98fc3d929c959f87023ab472d
Suneet Jain <44058062+suneet10@users.noreply.github.com>
2022-04-21 12:23:46 -0400

    API working

--- newcomponent_scanner/scannerfrontend/src/components/scannerTable/ScannerTable.jsx
+++ newcomponent_scanner/scannerfrontend/src/components/scannerTable/ScannerTable.jsx
@@ -28,7 +28,7 @@ function ScannerTable(props) {
                 <div className='chart-btn' onClick={() => setShowChart(true)}>Chart</div>
             </div>
             {showChart === true ? <Chart data={props.data.data} closePopup={closePopup} /> : null}
-            <div className='wrapper-table' id='piechart'>
+            <div className='wrapper-table'>
                 <table className='table'>
                     <thead className=''>
                         <tr className=''>
